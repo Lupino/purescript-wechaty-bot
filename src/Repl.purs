cfb81839c67f82ca9f9c41240bb311a89f213750
Lupino <lmjubuntu@gmail.com>
2018-04-13 10:45:06 +0800

    add whitelist and help

--- src/Repl.purs
+++ src/Repl.purs
@@ -1,4 +1,4 @@
-module Repl (launchRepl, ReplState, initReplState, showPrompt) where
+module Repl (launchRepl, ReplState, initReplState, checkWhitelist) where
 
 import Prelude
 
@@ -8,7 +8,7 @@ import Control.Monad.Eff.Console (log, CONSOLE)
 import Control.Monad.Eff.Exception (EXCEPTION, message)
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
-import Data.String (trim, drop, length, null)
+import Data.String (trim, drop, length, null, joinWith)
 import Node.ReadLine (Interface, READLINE, createConsoleInterface, noCompletion, setPrompt, setLineHandler, prompt)
 import Utils (startsWith)
 import Wechaty.Contact (find, getContactName, runContactT, say, Contact, self)
@@ -16,30 +16,29 @@ import Wechaty.Room (find, say) as R
 import Wechaty.Room (Room, getRoomTopic, runRoomT)
 import Wechaty.Types (WECHATY)
 import Control.Monad.Trans.Class (lift)
-import Control.Monad.Eff.Ref (REF, Ref, newRef, readRef, writeRef)
+import Control.Monad.Eff.Ref (REF, Ref, newRef, readRef, modifyRef)
 import Control.Monad.Reader (ReaderT, runReaderT, ask)
+import Data.Array (elem, (:), delete)
+import Data.Array (null) as A
 
-data ReplState = IsContact Interface Contact | IsRoom Interface Room | IsManager Interface Contact | Only Interface
+type Whitelist = Array String
+
+data ReplState = IsContact Whitelist Interface Contact | IsRoom Whitelist Interface Room | IsManager Whitelist Interface Contact | Only Whitelist Interface
 
 type Repl eff a = ReaderT (Ref ReplState) (Eff (ref :: REF, readline:: READLINE, console :: CONSOLE, exception :: EXCEPTION, wechaty :: WECHATY | eff)) a
 
 runRepl :: forall eff a. Ref ReplState -> Repl eff a -> Eff (ref :: REF, readline:: READLINE, console :: CONSOLE, exception :: EXCEPTION, wechaty :: WECHATY | eff) a
 runRepl s m = runReaderT m s
 
-initReplState :: forall eff. Eff (readline :: READLINE, console :: CONSOLE, exception :: EXCEPTION, wechaty :: WECHATY | eff) ReplState
+initReplState :: forall eff. Eff (ref :: REF, readline :: READLINE, console :: CONSOLE, exception :: EXCEPTION, wechaty :: WECHATY | eff) (Ref ReplState)
 initReplState = do
   rl <- createConsoleInterface noCompletion
-  pure $ Only rl
+  newRef $ Only [] rl
 
 get :: forall eff. Repl eff ReplState
 get = do
   ref <- ask
   lift $ readRef ref
-
-put :: forall eff. ReplState -> Repl eff Unit
-put ps = do
-  ref <- ask
-  lift $ writeRef ref ps
 
 setContactPrompt :: forall eff. Contact -> Interface -> Eff (readline :: READLINE | eff) Unit
 setContactPrompt c = setPrompt ps (length ps)
@@ -50,33 +49,60 @@ setRoomPrompt r = setPrompt ps (length ps)
   where ps = "Room<<" <> getRoomTopic r <> ">> "
 
 getInterface :: ReplState -> Interface
-getInterface (IsContact rl _) = rl
-getInterface (IsRoom rl _) = rl
-getInterface (IsManager rl _) = rl
-getInterface (Only rl) = rl
+getInterface (IsContact _ rl _) = rl
+getInterface (IsRoom _ rl _) = rl
+getInterface (IsManager _ rl _) = rl
+getInterface (Only _ rl) = rl
+
+getWhitelist :: ReplState -> Whitelist
+getWhitelist (IsContact w _ _) = w
+getWhitelist (IsRoom w _ _) = w
+getWhitelist (IsManager w _ _) = w
+getWhitelist (Only w _) = w
+
+putWhitelist :: Whitelist -> ReplState -> ReplState
+putWhitelist wl (IsContact _ rl c) = IsContact wl rl c
+putWhitelist wl (IsRoom _ rl c) = IsRoom wl rl c
+putWhitelist wl (IsManager _ rl c) = IsManager wl rl c
+putWhitelist wl (Only _ rl) = Only wl rl
+
+addWhitelist :: String -> Whitelist -> Whitelist
+addWhitelist xs wl | elem xs wl = wl
+                   | otherwise = xs : wl
+
+removeWhitelist :: String -> Whitelist -> Whitelist
+removeWhitelist xs = delete xs
+
+clearWhitelist :: String -> Whitelist -> Whitelist
+clearWhitelist _ _ = []
+
+replaceWhitelist :: forall eff. (String -> Whitelist -> Whitelist) -> String -> Repl eff Unit
+replaceWhitelist f xs = do
+  ref <- ask
+  lift $ modifyRef ref $ \ps -> putWhitelist (f xs (getWhitelist ps)) ps
+
+switch :: forall a eff. (Whitelist -> Interface -> a -> ReplState) -> a -> Repl eff Unit
+switch f a = do
+  ref <- ask
+  lift $ modifyRef ref $ \ps -> f (getWhitelist ps) (getInterface ps) a
 
 switchContact :: forall eff. Contact -> Repl eff Unit
-switchContact c = do
-  rl <- getInterface <$> get
-  lift $ setContactPrompt c rl
-  put (IsContact rl c)
+switchContact = switch IsContact
 
 switchManager :: forall eff. Repl eff Unit
 switchManager = do
   s <- lift self
-  rl <- getInterface <$> get
-  lift $ setContactPrompt s rl
-  put (IsManager rl s)
+  switch IsManager s
 
 switchRoom :: forall eff. Room -> Repl eff Unit
-switchRoom room = do
-  rl <- getInterface <$> get
-  lift $ setRoomPrompt room rl
-  put (IsRoom rl room)
+switchRoom room = switch IsRoom room
 
 data Cmd =
     FindContact String
   | FindRoom String
+  | AddWhitelist String
+  | RemoveWhitelist String
+  | ClearWhitelist
   | Msg String
   | Exit
   | Help
@@ -86,10 +112,24 @@ parseCmd :: String -> Cmd
 parseCmd xs
   | startsWith xs ".contact" = FindContact $ trim $ drop 8 xs
   | startsWith xs ".room" = FindRoom $ trim $ drop 5 xs
+  | startsWith xs ".whitelist add" = AddWhitelist $ trim $ drop 14 xs
+  | startsWith xs ".whitelist remove" = RemoveWhitelist $ trim $ drop 17 xs
+  | startsWith xs ".whitelist clear" = ClearWhitelist
   | startsWith xs ".exit" = Exit
   | startsWith xs ".help" = Help
   | null xs = Empty
   | otherwise = Msg xs
+
+help :: Array String
+help =
+  [ ".contact CONTACT          -- 切换用户"
+  , ".room ROOM                -- 切换聊天群"
+  , ".whitelist add STRING     -- 添加显示白名单"
+  , ".whitelist remove STRING  -- 移除显示白名单"
+  , ".whitelist clear          -- 清空显示白名单"
+  , ".exit                     -- 退出聊天, 跟自己聊"
+  , ".help                     -- 显示本帮助"
+  ]
 
 mkLineHandler
   :: forall eff. Ref ReplState -> (Cmd -> Repl eff Unit)
@@ -115,6 +155,7 @@ handlers (FindRoom n) = do
       (Right (Just c)) -> runRepl ref $ switchRoom c
       (Right Nothing) ->  log $ "Room<<" <> n <> ">> Not Found."
     showPrompt ps
+
 handlers (Msg m) = do
   ps <- get
   lift $ flip runAff_ (sendMessage ps m) $ \r -> do
@@ -123,26 +164,39 @@ handlers (Msg m) = do
       (Right _) -> pure unit
     showPrompt ps
 
-handlers Exit = do
-  switchManager
-  ps <- get
-  lift $ showPrompt ps
+handlers (AddWhitelist xs) = replaceWhitelist addWhitelist xs *> showPrompt_
+handlers (RemoveWhitelist xs) = replaceWhitelist removeWhitelist xs *> showPrompt_
+handlers ClearWhitelist = replaceWhitelist clearWhitelist "" *> showPrompt_
 
-handlers _ = do
-  ps <- get
-  lift $ showPrompt ps
+handlers Exit = switchManager *> showPrompt_
+
+handlers Help = do
+  lift $ log $ "\n" <> joinWith "\n" help
+  showPrompt_
+
+handlers Empty = showPrompt_
 
 sendMessage :: forall eff. ReplState -> String -> Aff (readline :: READLINE, console :: CONSOLE, exception :: EXCEPTION, wechaty :: WECHATY | eff) Unit
-sendMessage (IsContact _ c) = runContactT c <<< say
-sendMessage (IsRoom _ c) = runRoomT c <<< R.say
-sendMessage (IsManager _ c) = runContactT c <<< say
-sendMessage (Only _) = \_ -> pure unit
+sendMessage (IsContact _ _ c) = runContactT c <<< say
+sendMessage (IsRoom _ _ c) = runRoomT c <<< R.say
+sendMessage (IsManager _ _ c) = runContactT c <<< say
+sendMessage (Only _ _) = \_ -> pure unit
 
-launchRepl :: forall eff. ReplState -> Eff (ref :: REF, readline :: READLINE, console :: CONSOLE, exception :: EXCEPTION, wechaty :: WECHATY | eff) Unit
-launchRepl ps = do
-  ref <- newRef ps
+launchRepl :: forall eff. Ref ReplState -> Eff (ref :: REF, readline :: READLINE, console :: CONSOLE, exception :: EXCEPTION, wechaty :: WECHATY | eff) Unit
+launchRepl ref = do
   runRepl ref switchManager
+  ps <- readRef ref
   setLineHandler (getInterface ps) (mkLineHandler ref handlers)
+  showPrompt ps
 
 showPrompt :: forall eff. ReplState -> Eff (readline :: READLINE, console :: CONSOLE, exception :: EXCEPTION, wechaty :: WECHATY | eff) Unit
 showPrompt = prompt <<< getInterface
+
+showPrompt_ :: forall eff. Repl eff Unit
+showPrompt_ = lift <<< showPrompt =<< get
+
+checkWhitelist :: forall eff. Ref ReplState -> String -> Eff (ref :: REF | eff) Unit -> Eff (ref :: REF | eff) Unit
+checkWhitelist ref h io = do
+  wl <- getWhitelist <$> readRef ref
+  if A.null wl then io
+    else if elem h wl then io else pure unit
