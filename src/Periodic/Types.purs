7777636b08d34c942bb8f7aca4bd327a2494e574
Lupino <lmjubuntu@gmail.com>
2018-04-18 11:01:18 +0800

    sepreate packages

--- src/Periodic/Types.purs
+++
@@ -1,6 +0,0 @@
-module Periodic.Types
-  ( PERIODIC
-  ) where
-
-import Control.Monad.Eff (kind Effect)
-foreign import data PERIODIC :: Effect
--- src/Wechaty/Types.purs
+++
@@ -1,7 +0,0 @@
-module Wechaty.Types
-  ( WECHATY
-  ) where
-
-import Control.Monad.Eff (kind Effect)
-
-foreign import data WECHATY :: Effect
