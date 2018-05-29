4bd00c04fa2483752a04fe615b7ce7e4c9b86634
Lupino <lmjubuntu@gmail.com>
2018-05-29 14:52:35 +0800

    add remove room member

--- src/Chat.purs
+++ src/Chat.purs
@@ -6,12 +6,14 @@ module Chat
 
 import Prelude
 
-import Plan.Trans (PlanT, respond, param, ActionT, paramPattern)
+import Plan.Trans (PlanT, respond, param, ActionT, paramPattern, options, exit)
 import Effect.Aff (Aff)
 import Effect.Aff.Class (liftAff)
 import Data.String (trim, joinWith)
 import Wechaty.Contact (Contact) as C
+import Wechaty.Contact (getContactName)
 import Wechaty.Room (Room) as R
+import Wechaty.Room (memberAll, runRoomT, delete)
 import Data.Maybe (Maybe (..))
 import Data.FormURLEncoded (fromArray, encode)
 import Data.Tuple (Tuple (..))
@@ -43,8 +45,26 @@ searchHandler = do
   where go :: Json -> Array (Maybe String)
         go = caseJsonObject [] (\v -> [lookup "uri" v >>= toString, lookup "title" v >>= toString])
 
+removeRoomMember :: ActionM String
+removeRoomMember = do
+  opts <- options
+  name <- param "name"
+  case opts of
+    Room r true -> runRoomT r $ do
+      cs <- memberAll name
+      case cs of
+        [] -> pure "没有找到用户"
+        [c] -> do
+          delete c
+          pure "删除成功"
+        xs -> pure $ "找到:\n" <> (joinWith "\n" $ map getContactName xs)
+    _ -> exit
+
 launchChat :: ChatM Unit
 launchChat = do
   respond (paramPattern "search:keyword:") searchHandler
   respond (paramPattern "查找:keyword:") searchHandler
   respond (paramPattern "搜索:keyword:") searchHandler
+  respond (paramPattern "删除:name:") removeRoomMember
+  respond (paramPattern "rm:name:") removeRoomMember
+  respond (paramPattern "del:name:") removeRoomMember
