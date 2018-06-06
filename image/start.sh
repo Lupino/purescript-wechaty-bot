d54c709ef2723b3aa90cb216f9a57d2455db300d
Lupino <lmjubuntu@gmail.com>
2018-06-06 14:12:30 +0800

    log to stderr

--- image/start.sh
+++ image/start.sh
@@ -4,4 +4,4 @@ NAME=${NAME:-"demo"}
 SEARCH_HOST=${SEARCH_HOST:-"127.0.0.1:6000"}
 sed -i "s#config.default.DEFAULT_PROFILE#'$NAME'#" index.js
 sed -i "s#127.0.0.1:6000#$SEARCH_HOST#" index.js
-node index.js
+node index.js 2> stderr.log
