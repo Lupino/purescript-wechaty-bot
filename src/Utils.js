2e8aa8df151b46d6e7553499f59f7a5a8ebd1d41
Lupino <lmjubuntu@gmail.com>
2018-06-15 11:49:59 +0800

    eslint

--- src/Utils.js
+++ src/Utils.js
@@ -1,15 +1,15 @@
-var fetchJSON = require('higher-order-helper').fetchJSON;
+let fetchJSON = require('higher-order-helper').fetchJSON;
 
 exports.startsWith = function(s) {
   return function(s1) {
     return s.startsWith(s1);
-  }
-}
+  };
+};
 
 exports._fetchJSON = function(url) {
   return function(options) {
     return function() {
       return fetchJSON(url, options);
-    }
-  }
-}
+    };
+  };
+};
