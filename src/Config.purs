40ef9aa20a9e282a33d2e25b7ea86355d5973c63
Lupino <lmjubuntu@gmail.com>
2018-06-17 16:40:49 +0800

    update periodic config

--- src/Config.purs
+++ src/Config.purs
@@ -6,5 +6,5 @@ searchHost = "127.0.0.1:6000"
 dsn :: String
 dsn = "sqlite:bot.db"
 
-periodicHost :: {port :: Int}
-periodicHost = {port: 5000}
+periodicHost :: {port :: Int, host :: String}
+periodicHost = {port: 5000, host: "127.0.0.1"}
